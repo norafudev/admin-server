e55578dcce678a4fb477922eb823d6c62349421d
fuyachen <294143413@qq.com>
2023-04-07 22:07:46 +0800

    菜单新增、编辑、删除

--- routes/menus.js
+++ routes/menus.js
@@ -14,4 +14,30 @@ router.get("/list", async (ctx) => {
   ctx.body = util.success(rootList)
 })
 
+// 菜单新增、编辑、删除
+router.post("/operate", async (ctx) => {
+  const { action, _id, ...params } = ctx.request.body
+  let info
+  //   新增
+  try {
+    if (action == "add") {
+      const res = await Menu.create(params)
+      info = "创建成功"
+      // 编辑
+    } else if (action == "edit") {
+      const res = await Menu.findOneAndUpdate({ _id: _id }, params)
+      info = "编辑成功"
+      params.updateTime = new Date()
+      // 删除
+    } else {
+      const res = await Menu.findByIdAndRemove(_id)
+      await Menu.deleteMany({ parentId: { $all: [_id] } })
+      info = "删除成功"
+    }
+    ctx.body = util.success({}, info)
+  } catch (error) {
+    ctx.body = util.fail(`菜单操作失败：${error.stack}`)
+  }
+})
+
 module.exports = router
