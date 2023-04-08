852de71b49a0948f37ffeaaefcc7b312169a47d8
fuyachen <294143413@qq.com>
2023-04-08 13:44:19 +0800

    菜单列表递归实现

--- routes/menus.js
+++ routes/menus.js
@@ -10,27 +10,31 @@ router.get("/list", async (ctx) => {
   const params = {}
   if (menuName) params.menuName = menuName
   if (menuState) params.menuState = menuState
-  let rootList = (await Menu.find(params)) || []
-  const permissionList = getTreeMenu(rootList, null, [])
-  ctx.body = util.success(permissionList)
+  let menuList = (await Menu.find(params)) || []
+  const treeMenu = getTreeMenu(menuList, null, [])
+  ctx.body = util.success(treeMenu)
 })
 
 // 递归拼接树形列表
-function getTreeMenu(rootList, id, list) {
-  for (let i = 0; i < rootList.length; i++) {
-    let item = rootList[i]
-    if (String(item.parentId.slice().pop()) == String(id)) {
-      list.push(item._doc)
+function getTreeMenu(menuList, parentId, list) {
+  console.log("进入递归")
+  //pi数组的最后一个元素等于我们传入的id，就推进数组
+  menuList.forEach((menu) => {
+    // !!!数据库中的id都是 ObjectID 类型的对象，需要转成字符串，否则比较的就是对象的内存地址
+    if (String(menu.parentId[menu.parentId.length - 1]) == String(parentId)) {
+      list.push(menu._doc)
+      console.log("menu._doc======>", menu._doc)
     }
-  }
-  list.map((item) => {
-    item.children = []
-    getTreeMenu(rootList, item._id, item.children)
-    if (item.children.length == 0) {
-      delete item.children
-    } else if (item.children.length > 0 && item.children[0].menuType == 2) {
+  })
+  // 给上级菜单添加children属性
+  list.forEach((menu) => {
+    menu.children = []
+    getTreeMenu(menuList, menu._id, menu.children)
+    if (menu.children.length == 0) {
+      delete menu.children
+    } else if (menu.children.length > 0 && menu.children[0].menuType == 2) {
       // 快速区分按钮和菜单，用于后期做菜单按钮权限控制
-      item.action = item.children
+      menu.action = menu.children
     }
   })
   return list
